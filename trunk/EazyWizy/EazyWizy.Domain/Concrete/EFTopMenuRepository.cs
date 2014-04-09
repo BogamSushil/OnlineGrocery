2fcf5852a336752de94c2cb223f8c8b17979ec6f
AV <ahv1982@gmail.com>
2014-04-09 22:45:18 +0530

    Added: Web UI Project

---
+++ trunk/EazyWizy/EazyWizy.Domain/Concrete/EFTopMenuRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using EazyWizy.Domain.Abstract;
+using EazyWizy.Domain.Entities;
+
+namespace EazyWizy.Domain.Concrete
+{
+    public class EFTopMenuRepository : ITopMenuRepository    
+    {
+        private EazyWizyEntities ewEntities = new EazyWizyEntities();        
+        IEnumerable<Entities.TopMenu> ITopMenuRepository.TopMenu
+        {
+            get {
+               return ewEntities.sp_Select_GetMenu();
+            }
+        }
+    }
+}
---
+++ trunk/EazyWizy/EazyWizy.WebUI/Controllers/TopMenuController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using EazyWizy.Domain.Abstract;
+
+
+namespace EazyWizy.WebUI.Controllers
+{
+    public class TopMenuController : Controller
+    {
+
+    }
+}
